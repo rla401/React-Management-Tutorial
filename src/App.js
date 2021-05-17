939fc3352a0c5f309443eb63be5888b570d299bb
DoHyungKim <dpdeh123@gmail.com>
2021-05-17 18:27:58 +0900

    Create Cistomer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import logo from './logo.svg';
+import Customer from './components/Customer'
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!!</h2>
-    </div>
-  );
+const customers =[{
+  'id': '1',
+  'image':'https://placeimg.com/64/64/1',
+  'name':'김도형',
+  'birthday':'901010',
+  'gender' : '남자',
+  'job' : '개발자'
+},
+{
+  'id': '2',
+  'image':'https://placeimg.com/64/64/2',
+  'name':'이수댁',
+  'birthday':'940702',
+  'gender' : '남자',
+  'job' : '취준생'
+}
+,
+{
+  'id': '3',
+  'image':'https://placeimg.com/64/64/3',
+  'name':'엥도',
+  'birthday':'981230',
+  'gender' : '남자',
+  'job' : '대학생'
+}
+]
+class App extends Component{
+  render(){
+   return(
+     <div>
+       {
+         customers.map(c => {
+           return (<Customer
+           id={c.id}
+           image={c.image}
+           name={c.name}
+           birthday={c.birthday}
+           gender={c.gender}
+           job={c.job}
+           />
+         );
+         })
+       }
+     </div>
+   );
+  }
 }
 
 export default App;
